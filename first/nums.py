c3ac9f0c45df8db045dd898758718d9c9c4eefa0
dykrbo <89838886+dykrbo@users.noreply.github.com>
2022-07-15 22:07:48 +0300

    Добавлена функция mult добавлен модуль string

--- first/nums.py
+++ first/nums.py
@@ -2,9 +2,13 @@ def plus(a,b):
     return a+b
 
 
-def minus(a,b)
+def multi(a,b):
+    return a*b
+
+def minus(a,b):
     return a-b
 
 if __name__ == '__main__':
     print(plus(2,2))
-    print(minus(2, 2))
+    print(minus(2,2))
+    print(multi(2,2))
